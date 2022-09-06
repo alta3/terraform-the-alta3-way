caf7677aab5f9555eb81d0aaac574f0eb005f662
seaneon <35880398+seaneon@users.noreply.github.com>
2022-09-06 13:06:04 -0400

    Create terraform_docker_variables_main.tf

---
+++ labs/terraform_docker_variables_main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  # here we removed the name "tutorial" for the container
+  # and replace it with a call to a variable
+  # name  = "tutorial"
+  name = var.container_name
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
