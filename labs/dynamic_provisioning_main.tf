49e2a272d059a2a39080ad70ed2a1f3ddbbc72e2
seaneon <35880398+seaneon@users.noreply.github.com>
2022-09-06 14:52:57 -0400

    Create dynamic_provisioning_main.tf

---
+++ labs/dynamic_provisioning_main.tf
@@ -0,0 +1,29 @@
+# updated main.tf
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  # here we removed the name "tutorial" for the container
+  # and replace it with a call to a variable
+  # name  = "tutorial"
+  name = var.container_name
+  ports {
+    # internal and external are now defined by variables
+    internal = var.internal_port
+    external = var.external_port
+  }
+}
