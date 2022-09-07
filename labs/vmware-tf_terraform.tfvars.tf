a56404d83297e3f3108b7884f9d95dcbc9d3c436
seaneon <35880398+seaneon@users.noreply.github.com>
2022-09-07 16:17:17 -0400

    Create vmware-tf_terraform.tfvars.tf

---
+++ labs/vmware-tf_terraform.tfvars.tf
@@ -0,0 +1,8 @@
+vsphere_server     = ""                             //provide at run time
+vsphere_user       = "Administrator@vsphere.local"
+vsphere_password   = ""                             //provide at run time
+datacenter         = "Alta3Datacenter"
+datastore          = "datastore1"
+cluster            = ""                             //provide at run time
+network_name       = "VM Network"
+ubuntu_name        = "tf-edu-ubuntu"
