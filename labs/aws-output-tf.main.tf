53c56a0bf6cd46ba19e18474ccd2683f4eae0c7b
seaneon <35880398+seaneon@users.noreply.github.com>
2022-09-07 13:49:44 -0400

    Create aws-output-tf.main.tf

---
+++ labs/aws-output-tf.main.tf
@@ -0,0 +1,27 @@
+/* main.tf
+   Deploy a small ec2 instance on AWS */
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
