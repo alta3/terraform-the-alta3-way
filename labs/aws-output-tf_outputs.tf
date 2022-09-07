c82aa85a4a8a86397597990c8e5bfc265a093169
seaneon <35880398+seaneon@users.noreply.github.com>
2022-09-07 14:53:22 -0400

    Create aws-output-tf_outputs.tf

---
+++ labs/aws-output-tf_outputs.tf
@@ -0,0 +1,12 @@
+/* outputs.tf
+   outputs to display */
+   
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
