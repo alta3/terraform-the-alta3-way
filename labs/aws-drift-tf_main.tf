3c0d9c00001d9aebeb4dada54018a4dcfac88822
seaneon <35880398+seaneon@users.noreply.github.com>
2022-09-07 15:41:11 -0400

    Create aws-drift-tf_main.tf

---
+++ labs/aws-drift-tf_main.tf
@@ -0,0 +1,74 @@
+/* main.tf
+   Launch a EC2 instance with a keypair, security group for SSHing, a keypair */
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.24.1"
+    }
+  }
+  required_version = ">= 0.15"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  # use the file() function to lookup data
+  public_key = file("~/.ssh/key.pub")
+}
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.ubuntu.id
+  key_name               = aws_key_pair.deployer.key_name
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg_ssh.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+  tags = {
+    Name          = "terraform-learn-state-ec2"
+    drift_example = "v1"
+  }
+}
+
+resource "aws_security_group" "sg_ssh" {
+  name = "sg_ssh"
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
