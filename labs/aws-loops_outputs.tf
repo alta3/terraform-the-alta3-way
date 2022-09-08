6b912e484b42f6489cee3634ec5c217cd493f29a
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2022-09-08 11:49:44 -0400

    Update aws-loops_outputs.tf

--- labs/aws-loops_outputs.tf
+++ labs/aws-loops_outputs.tf
@@ -1,9 +1,4 @@
-output "neo_arn" {
-  value       = aws_iam_user.example[0].arn
-  description = "The ARN for user romeo"
-}
-
 output "all_arns" {
-  value       = aws_iam_user.example[*].arn
+  value       = aws_iam_user.example[*]
   description = "The ARNs for all users"
 }
